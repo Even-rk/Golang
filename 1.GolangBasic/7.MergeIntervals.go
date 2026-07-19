f2ebbb1377e0b6b307a6f253b6597f4ae2894515
Even <kwr011024@163.com>
2026-07-19 13:02:12 +0800

    refactor: 替换手动排序为标准库并优化区间合并逻辑
    使用Go标准库sort.Slice替换手动冒泡排序，提升排序效率
    修复合并区间时未取最大结束值的错误
    更新注释并简化合并条件判断

--- 1.GolangBasic/7.MergeIntervals.go
+++ 1.GolangBasic/7.MergeIntervals.go
@@ -1,25 +1,19 @@
 package main
 
+import "sort"
+
 // 合并区间
 func merge(intervals [][]int) [][]int {
-	// 先做一次排序
-	// 谁的开始位最小，谁在前
-	for i := 0; i < len(intervals)-1; i++ {
-		// 如果当前区间的开始位置 大于 后一个区间的开始位, 两个区间交换
-		if intervals[i][0] > intervals[i+1][0] {
-			// 交换区间
-			intervals[i], intervals[i+1] = intervals[i+1], intervals[i]
-			// 交换后，回到第一个区间继续判断
-			i = -1
-			continue
-		}
-	}
+	// 导入sort包后使用标准库排序，按区间的起始位置升序排列
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 
 	// 遍历切片
 	for i := 0; i < len(intervals)-1; i++ {
 		// 判断前一个区间的结束位和后一个区间的开始位
 		// 创建新的区间，开始位使用前一个区间的开始，结束位使用后一个位区间的结束
-		// 1，前者大于后者， 2，前者➕1 等于后者，3，前者等于后者
+		// 1，前者大于后者， 2，前者等于后者
 
 		// 前者的开始位和结束位
 		preStart := intervals[i][0]
@@ -27,10 +21,10 @@ func merge(intervals [][]int) [][]int {
 		// 后者的开始位和结束位
 		nextStart := intervals[i+1][0]
 		nextEnd := intervals[i+1][1]
-		// 判断 是否符合 123条件
-		if preEnd > nextStart || preEnd+1 == nextStart || preEnd == nextStart {
+		// 判断 是否符合 12条件
+		if preEnd > nextStart || preEnd == nextStart {
 			// 符合条件，创建一个新的区间
-			newInterval := []int{preStart, nextEnd}
+			newInterval := []int{preStart, max(preEnd, nextEnd)}
 			// 合并区间, 利用切片删除这两个区间在在原来的位置把新的区间添加
 			intervals = append(intervals[:i], append([][]int{newInterval}, intervals[i+2:]...)...)
 			i = -1 // 跳出当前循环，重新判断
