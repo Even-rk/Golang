07d80a25a1382e5d4c521b585850dcb246a15b05
Even <kwr011024@163.com>
2026-07-19 13:11:47 +0800

    fix(removeDuplicates): 修复函数缺陷并优化去重实现
    修改入参为切片指针以修改原切片，改用相邻元素比较去重保留原顺序，同步更新函数注释

--- 1.GolangBasic/6.RemoveDuplicates.go
+++ 1.GolangBasic/6.RemoveDuplicates.go
@@ -1,17 +1,17 @@
 package main
 
-// 删除重复项
-func removeDuplicates(nums []int) int {
-	// 创建map 记录每个元素出现的次数
-	m := make(map[int]int)
-	for _, v := range nums {
-		m[v]++
+// 删除重复项，保留原有的顺序,入参使用指针，需要操作原切片
+func removeDuplicates(nums *[]int) int {
+	// 遍历切片
+	for i := 0; i < len(*nums)-1; i++ {
+		// 判断当前元素是否是重复的
+		if (*nums)[i] == (*nums)[i+1] {
+			// 删除重复的元素
+			*nums = append((*nums)[:i], (*nums)[i+1:]...)
+			i = -1 // 跳出当前循环，重新判断
+			continue
+		}
 	}
-	// 清空切片
-	nums = nums[:0]
-	// 遍历key 到切片
-	for k := range m {
-		nums = append(nums, k)
-	}
-	return len(nums)
+
+	return len(*nums)
 }
